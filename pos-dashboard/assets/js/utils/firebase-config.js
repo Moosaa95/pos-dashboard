56400fb8ce551bd6454e9f4937b716bcfe38d461
ameenaomaar <ameenaomaar26@gmail.com>
2025-05-21 07:26:44 -0700

    ...

---
+++ pos-dashboard/assets/js/utils/firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+
+// Import the functions you need from the SDKs (if using modules - optional)
+var config = {
+        apiKey: "AIzaSyD9NtJjMVVyS4DHd5t7R_WayPJTnkiylmI",
+        authDomain: "pos-b43f8.firebaseapp.com",
+        databaseURL: "https://pos-dashboard.firebaseio.com",
+        // ... other configuration options
+    };
+    firebase.initializeApp(config);
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
